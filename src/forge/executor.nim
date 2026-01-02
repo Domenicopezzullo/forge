8527a069e8be975f71be7c21e9cba4f584752451
Domenicopezzullo <pezzullodomenico712@gmail.com>
2026-01-02 16:12:13 +0100

    hello

--- src/forge/executor.nim
+++ src/forge/executor.nim
@@ -3,6 +3,7 @@ import osproc, times, os, lexer, tables, strutils
 proc needsRebuild*(task: TaskDef, taskMap: Table[string, TaskDef]): bool =
     let targetIsFile = fileExists(task.name)
 
+
     if not targetIsFile and task.dependencies.len == 0:
         return true
 
